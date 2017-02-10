630009a93fa084cb821b51fd480662c85dd646ae
Edwin Torres <ERTORRES@MITRE.ORG>
2017-02-10 16:42:21 -0500

    this is release v1.1

---
+++ RELEASENOTES.md
@@ -0,0 +1,5 @@
+Release Tag: 20161027qa
+ACR-CDR 
+* gets a list of all cdr records from the cdr database
+* gets a list of cdr events between a date range
+* returns records as a json object or csv file
--- routes/routes.js
+++ routes/routes.js
@@ -1,45 +1,92 @@
 // Define the different REST service routes in this file.
 var json2csv = require('json2csv');
-var csvFields = ['calldate', 'clid', 'src',
-              'dst', 'dcontext', 'channel',
-              'dstchannel', 'lastapp', 'lastdata',
-              'duration', 'billsec', 'disposition',
-              'amaflags', 'accountcode', 'userfield',
-              'uniqueid', 'linkedid', 'sequence',
-              'peeraccount'];
-var appRouter = function(app,connection) {
 
-    // This is just for testing to make sure the server is accessible
-    // GET request; e.g. http://localhost:8086/
+var appRouter = function(app,connection,logger, cdrTable) {
+
+    /**
+	 * Get to show server is running. This will not show if APIDoc is run.
+   */
     app.get('/', function(req, res) {
+		logger.info("GET /");
         return res.status(200).send({'message': 'Welcome to the CDR report portal.'});
     });
 
-    // GETALLCDRRECS - Returns all records in the table
-    //GET request; e.g. http://localhost:8086/getallcdrrecs
+    /**
+   * @api {get} /getallcdrrecs Get all CDR database records
+   * @apiName GetAllCDRRecs
+   * @apiGroup GetAllCDRRecs
+   * @apiVersion 1.0.0
+   *
+   * @apiParam {Date} [start]          Start date for cdr records (format YYYY-MM-DD)
+   * @apiParam {Date} [end]            End date for cdr records (format YYYY-MM-DD)
+   * @apiParam {String} [format]      Format results are returned in. Defaults to JSON, accepts csv.
+   *
+   * @apiSuccessExample Success-Response:
+   *     HTTP/1.1 200 OK
+   *    {
+   *      "message":"Success",
+   *      "data":[
+   *           {
+   *              "calldate":"2016-08-24T02:05:30.000Z",
+   *              "clid":"\"\" <6004>",
+   *              "src":"6004",
+   *              "dst":"agents",
+   *              "dcontext":"from-internal",
+   *              "channel":"SIP/6004-00000000",
+   *              "dstchannel":"",
+   *              "lastapp":"Playback",
+   *              "lastdata":"moh14",
+   *              "duration":14,
+   *              "billsec":14,
+   *              "disposition":"ANSWERED",
+   *              "amaflags":3,
+   *              "accountcode":"",
+   *              "userfield":"",
+   *              "uniqueid":"1471979130.0",
+   *              "linkedid":"1471979130.0",
+   *              "sequence":"0",
+   *              "peeraccount":""
+   *            }]
+   *    }
+   * @apiErrorExample Error-Response
+   *     HTTP/1.1 500 Internal Server Error
+   *     {
+   *        'message': 'MySQL error'
+   *     }
+   */
     app.get('/getallcdrrecs', function(req, res) {
-        console.log("CALL: /getallcdrrecs");
-	var query = 'SELECT * FROM bit_cdr ORDER BY calldate';
+		logger.info("GET /getallcdrrecs");
+        var query = 'SELECT * FROM ' + cdrTable + ' ORDER BY calldate';
         if(req.query.start && req.query.end){
-            query = 'SELECT * FROM bit_cdr WHERE (calldate BETWEEN ' + connection.escape(req.query.start) + ' AND ' + connection.escape(req.query.end) + ')';           
+            query = 'SELECT * FROM ' + cdrTable + ' WHERE (calldate BETWEEN ' + connection.escape(req.query.start) + ' AND ' + connection.escape(req.query.end) + ')';
         }
-	console.log("Query: " + query);        
         // Query for all records sorted by the id
         connection.query(query, function(err, rows, fields) {
             if (err) {
-                console.log("ERROR: "+err);
+                //console.log(err);
+				logger.error("/getallcdrrecs an error has occurred");
                 return res.status(500).send({'message': 'MySQL error'});
             }
-            else if (rows.length >= 0) {            	
-		    //success           
-		console.log("Results returned");     
-                if(req.query.format == 'csv'){
+            else if (rows.length > 0) {
+                //success
+                if(req.query.format === 'csv') {
                     JSON.stringify(rows);
+
+                    // Column names for the CSV file.
+          					var csvFields = ['calldate', 'clid', 'src',
+          						'dst', 'dcontext', 'channel',
+          						'dstchannel', 'lastapp', 'lastdata',
+          						'duration', 'billsec', 'disposition',
+          						'amaflags', 'accountcode', 'userfield',
+          						'uniqueid', 'linkedid', 'sequence',
+          						'peeraccount'];
+
                     var csv = json2csv({data: rows, fields: csvFields});
                     res.setHeader('Content-disposition', 'attachment; filename=cdr.csv');
                     res.set('Content-Type', 'text/csv');
                     res.status(200).send(csv);
-                }else{
+                }
+                else {
                     res.status(200).send({'message': 'Success', 'data':rows});
                 }
             }
@@ -47,40 +94,6 @@ var appRouter = function(app,connection) {
                 return res.status(200).send({'message': 'No cdr records', 'data':rows});
             }
         });
-    });
-
-    // CDRFILTER - Returns all records within the specified time period
-    //GET request; e.g. http://localhost:8086/cdrfilter/?start=2016-08-17 12:21:31&end=2016-08-17 17:00:00
-    app.get('/cdrfilter', function(req, res) {
-
-        if (!req.query.start || !req.query.end) {
-            return res.status(400).send({'message': 'Missing required start or end time(s)'});
-        }
-        else {
-            // Note, use connection.escape to avoid SQL injection attacks
-            var sql = 'SELECT * FROM bit_cdr WHERE (calldate BETWEEN ' + connection.escape(req.query.start) + ' AND ' + connection.escape(req.query.end) + ')';
-            connection.query(sql, function(err, rows, fields)  {
-
-                // Working uglier alternative
-                // connection.query('SELECT * FROM cdr_log WHERE (calldate BETWEEN "' + req.query.start + '" AND "' + req.query.end + '")', function(err, rows, fields)  {
-
-                if (err) {
-                    console.log(err);
-                    return res.status(500).send({'message': 'MySQL error'});
-                }
-                else if (rows.length >= 1) {
-                    //success
-                    // json = JSON.stringify(rows);
-                    res.status(200).send({'message': 'Success', 'data':rows});
-                }
-                else if (rows.length === 0) {
-                    return res.status(404).send({'message': 'CDR records not found'});
-                }
-                else {
-                    return res.status(501).send({'message': 'Unknown error'});
-                }
-            });
-        }
     });
 };
 
